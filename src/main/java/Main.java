47e00c34c49a27c7373179a287e24662e86cdab6
anamaksimova <19cherrytree91@gmail.com>
2021-11-08 19:04:07 +0500

    l5-0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,10 +26,30 @@ public class Main {
 //        long m1 = System.currentTimeMillis();
 //        System.out.println("Выбором сортировка "+ (m1-m) + " ms");
 
-        long m = System.currentTimeMillis();
-        mal.bubbleSortOpt();
-        long m1 = System.currentTimeMillis();
-        System.out.println("Пузырьковая опт сортировка "+ (m1-m) + " ms");
+//        long m = System.currentTimeMillis();
+//        mal.bubbleSortOpt();
+//        long m1 = System.currentTimeMillis();
+//        System.out.println("Пузырьковая опт сортировка "+ (m1-m) + " ms");
+//
+//        Random random = new Random();
+//        int n = 1000000;
+//        MyArrayList<Integer> mal = new MyArrayList<>(n);
+//        for (int i = 0; i < n; i++) {
+//            mal.add(random.nextInt(100000));
+//        }
+
+        long begin = System.currentTimeMillis();
+//        System.out.println(mal);
+
+//        mal.selectionSort();//15091 ms
+//        mal.insertionSort();//5836 ms
+//        mal.bubbleSort();//37917 ms
+//        mal.bubbleSortOpt();
+        mal.quickSort();//83 ms
+//        System.out.println(mal);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time: %d ms", end - begin);
+
 
     }
 }
